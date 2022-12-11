28e713b59a88c92a166f9e364c69236fcb689ba2
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-12-11 00:44:35 -0800

    Converted all components into functional components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component.jsx";
 import SearchBox from "./components/search-box/searchbox-component.jsx";
 const App = () => {
-  const [searchField, setSearchField] = useState("a"); // [value, setValue]
+  const [searchField, setSearchField] = useState(""); // [value, setValue]
   const [monsters, setMonsters] = useState([]);
   const [fm, setfm] = useState([monsters]);
 
@@ -39,49 +38,4 @@ const App = () => {
   );
 };
 
-// class App extends Component {
-//   constructor() {
-//     console.log("render from App.js");
-//     super();
-//     this.state = {
-//       monsters: [],
-//       searchField: "",
-//     };
-//   }
-//   componentDidMount() {
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then((response) => response.json())
-//       .then((users) =>
-//         this.setState(() => {
-//           return { monsters: users };
-//         })
-//       );
-//   }
-//   searching = (event) => {
-//     const searchField = event.target.value.toLocaleLowerCase();
-//     this.setState(() => {
-//       return { searchField };
-//     });
-//   };
-
-//   render() {
-//     console.log("render from App.js");
-//     const { monsters, searchField } = this.state;
-//     const { searching } = this;
-//     const fm = monsters.filter((monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
-//     return (
-//       <div className="App">
-//         <h1 className="app-title">Monsters Rolodex</h1>
-//         <SearchBox
-//           onChangeHandler={searching}
-//           placeholder="search monsters"
-//           className="monsters-search-box"
-//         />
-//         <CardList monsters={fm} />
-//       </div>
-//     );
-//   }
-// }
 export default App;
