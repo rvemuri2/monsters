f3b18c03eb9f7e9bd3d5cc167538a6c8c51cfcfa
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-12-10 23:46:59 -0800

    Created Monsters Rolodex web app with class components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import CardList from "./components/card-list/card-list.component.jsx";
+import SearchBox from "./components/search-box/searchbox-component.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    console.log("render from App.js");
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+  searching = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    console.log("render from App.js");
+    const { monsters, searchField } = this.state;
+    const { searching } = this;
+    const fm = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    return (
+      <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
+        <SearchBox
+          onChangeHandler={searching}
+          placeholder="search monsters"
+          className="monsters-search-box"
+        />
+        <CardList monsters={fm} />
+      </div>
+    );
+  }
 }
-
 export default App;
